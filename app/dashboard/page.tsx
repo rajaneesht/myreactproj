4da84cece87c02916eb4f541fd9d3380bee0e254
Rajaneesh T <rajaneet@amazon.com>
2024-05-09 21:46:13 +0530

    made changes to nav order

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -22,7 +22,7 @@
     return (
       <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-          Dashboard
+          My Home View
         </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
           {<Card title="Collected" value={totalPaidInvoices} type="collected" /> }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,9 +13,9 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Living Room', href: '/dashboard/invoices', icon: DocumentDuplicateIcon, },
+  { name: 'Routines', href: '/dashboard/routines', icon: DocumentDuplicateIcon },
+  { name: 'Living Room', href: '/dashboard/invoices', icon: UserGroupIcon, },
   { name: 'Kitchen', href: '/dashboard/customers', icon: UserGroupIcon },
-  { name: 'Routines', href: '/dashboard/routines', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
@@ -31,7 +31,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-green-100 text-pink-600': pathname === link.href,
+                'bg-blue-100 text-white-600': pathname === link.href,
               },
             )}
             // className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
